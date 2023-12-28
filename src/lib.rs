a82a74de6f02e9ef0cb913e3a7e79f4a1975f111
Alexander Cyon <alex.cyon@gmail.com>
2023-12-28 15:24:57 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,132 +1,139 @@
 #[allow(unused)]
+use std::{
+    cell::RefCell,
+    ops::Deref,
+    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
+};
 
-mod u0 {
-    use std::{
-        cell::RefCell,
-        ops::Deref,
-        sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
-    };
+use uuid::Uuid;
 
-    use uuid::Uuid;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, uniffi::Enum)]
+pub enum Environment {
+    Prod,
+    Test,
+}
 
-    #[derive(Debug, Clone, PartialEq, Eq, uniffi::Enum)]
-    pub enum Environment {
-        Prod,
-        Test,
-    }
-
-    #[derive(Debug, Clone, PartialEq, Eq, uniffi::Record)]
-    pub struct User {
-        id: String,
-        name: String,
-    }
-    impl User {
-        pub fn new(name: &str) -> Self {
-            Self {
-                id: Uuid::new_v4().to_string(),
-                name: name.to_string(),
-            }
-        }
-        pub fn get_name(&self) -> String {
-            self.name.clone()
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Record)]
+pub struct User {
+    id: String,
+    name: String,
+}
+impl User {
+    pub fn new(name: &str) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            name: name.to_string(),
         }
     }
-
-    #[derive(Debug, Clone, PartialEq, Eq, uniffi::Record)]
-    pub struct Users {
-        environment: Environment,
-        users: Vec<User>,
+    pub fn get_name(&self) -> String {
+        self.name.clone()
     }
-    impl Users {
-        pub fn new(environment: Environment) -> Users {
-            Self {
-                environment,
-                users: Vec::new(),
-            }
-        }
+}
 
-        pub fn len(&self) -> usize {
-            self.users.len()
-        }
-
-        pub fn add_user(&mut self, named: &str) {
-            self.users.push(User::new(named))
-        }
-
-        pub fn change_name(&mut self, user_index: usize, to: String) {
-            self.users[0].name = to
+#[derive(Debug, Clone, PartialEq, Eq, uniffi::Record)]
+pub struct Users {
+    environment: Environment,
+    users: Vec<User>,
+}
+impl Users {
+    pub fn new(environment: Environment) -> Users {
+        Self {
+            environment,
+            users: Vec::new(),
         }
     }
 
-    #[uniffi::export]
-    pub fn new_users(environment: Environment) -> Users {
-        Users::new(environment)
+    pub fn len(&self) -> usize {
+        self.users.len()
     }
 
-    #[derive(Debug, uniffi::Object)]
-    pub struct Holder {
-        users: RwLock<Users>,
+    pub fn add_user(&mut self, named: &str) {
+        self.users.push(User::new(named))
     }
 
-    impl Holder {
-        fn read<T: Clone, F>(&self, access: F) -> T
-        where
-            F: Fn(RwLockReadGuard<'_, Users>) -> T,
-        {
-            access(self.users.try_read().unwrap())
-        }
+    pub fn change_name(&mut self, user_index: usize, to: String) {
+        self.users[user_index].name = to
+    }
+}
 
-        fn write<F>(&self, mutate: F)
-        where
-            F: Fn(RwLockWriteGuard<'_, Users>),
-        {
-            mutate(self.users.try_write().unwrap())
-        }
+#[uniffi::export]
+pub fn new_users(environment: Environment) -> Users {
+    Users::new(environment)
+}
 
-        fn len(&self) -> usize {
-            self.read(|u| u.len())
-        }
+#[derive(Debug, uniffi::Object)]
+pub struct Holder {
+    users: RwLock<Users>,
+}
+
+impl Holder {
+    fn read<T: Clone, F>(&self, access: F) -> T
+    where
+        F: Fn(RwLockReadGuard<'_, Users>) -> T,
+    {
+        access(self.users.try_read().unwrap())
     }
 
-    #[uniffi::export]
-    impl Holder {
-        #[uniffi::constructor]
-        pub fn new(environment: Environment) -> Arc<Self> {
-            Arc::new(Self {
-                users: RwLock::new(Users::new(environment)),
-            })
-        }
+    fn write<F>(&self, mutate: F)
+    where
+        F: Fn(RwLockWriteGuard<'_, Users>),
+    {
+        mutate(self.users.try_write().unwrap())
+    }
 
-        pub fn add_user(&self, named: String) {
-            self.write(|mut u| u.add_user(named.as_str()))
-        }
+    fn len(&self) -> usize {
+        self.read(|u| u.len())
+    }
+}
 
-        pub fn change_name_of_user(&self, at: u32, to: String) {
-            self.write(|mut u| u.change_name(at as usize, to.to_owned()))
-        }
+#[uniffi::export]
+impl Holder {
+    #[uniffi::constructor]
+    pub fn new(environment: Environment) -> Arc<Self> {
+        Arc::new(Self {
+            users: RwLock::new(Users::new(environment)),
+        })
+    }
 
-        pub fn user_count(&self) -> u32 {
-            self.len() as u32
-        }
-        pub fn get_users(&self) -> Vec<User> {
-            self.read(|u| u.users.clone())
-        }
+    pub fn add_user(&self, named: String) {
+        self.write(|mut u| u.add_user(named.as_str()))
+    }
+
+    pub fn change_name_of_user(&self, at: u32, to: String) {
+        self.write(|mut u| u.change_name(at as usize, to.to_owned()))
+    }
+
+    pub fn user_count(&self) -> u32 {
+        self.len() as u32
+    }
+    pub fn get_users(&self) -> Vec<User> {
+        self.read(|u| u.users.clone())
     }
 }
 
 uniffi::include_scaffolding!("users");
 
 #[cfg(test)]
-mod test_u0 {
-    use crate::u0::{Environment, Holder};
+mod tests {
+    use crate::{Environment, Holder};
 
     #[test]
-    fn u0() {
+    fn test() {
         let holder = Holder::new(Environment::Prod);
         assert_eq!(holder.user_count(), 0);
         holder.add_user("Foo".to_string());
         assert_eq!(holder.get_users()[0].get_name(), "Foo");
         holder.change_name_of_user(0, "Bar".to_string());
         assert_eq!(holder.get_users()[0].get_name(), "Bar");
+        holder.add_user("Biz".to_string());
+        holder.change_name_of_user(1, "Buz".to_string());
+        assert_eq!(
+            holder
+                .get_users()
+                .into_iter()
+                .map(|u| u.get_name())
+                .collect::<Vec<String>>(),
+            ["Bar", "Buz"]
+        );
     }
 }
--- tests/test_swift.swift
+++ tests/test_swift.swift
@@ -10,6 +10,9 @@ func test() throws {
   assert(holder.getUsers()[0].name == "Foo")
   holder.changeNameOfUser(at: 0, to: "Bar")
   assert(holder.getUsers()[0].name == "Bar")
+  holder.addUser(named: "Biz")
+  holder.changeNameOfUser(at: 1, to: "Buz")
+  assert(holder.getUsers().map(\.name) == ["Bar", "Buz"])
 }
 
 try! test()
