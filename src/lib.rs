fefb073f8f0e83964affe13d075296ef43cf5d9e
Alexander Cyon <alex.cyon@gmail.com>
2023-12-28 14:46:20 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #[allow(unused)]
 
 mod u0 {
+    use std::{
+        cell::RefCell,
+        ops::Deref,
+        sync::{Arc, RwLock, RwLockReadGuard},
+    };
+
     use uuid::Uuid;
 
     #[derive(Debug, Clone, PartialEq, Eq, uniffi::Enum)]
@@ -29,23 +35,60 @@ mod u0 {
         users: Vec<User>,
     }
     impl Users {
-        pub fn new<I>(environment: Environment, users: I) -> Self
-        where
-            I: IntoIterator<Item = User>,
-        {
+        pub fn new(environment: Environment) -> Users {
             Self {
                 environment,
-                users: users.into_iter().collect(),
+                users: Vec::new(),
             }
         }
-        pub fn empty_prod() -> Self {
-            Self::new(Environment::Prod, [])
-        }
-        pub fn new_user(&mut self, name: &str) {
-            self.users.push(User::new(name))
-        }
+
         pub fn len(&self) -> usize {
             self.users.len()
+        }
+
+        pub fn add_user(&mut self, named: &str) {
+            self.users.push(User::new(named))
+        }
+    }
+
+    #[uniffi::export]
+    pub fn new_users(environment: Environment) -> Users {
+        Users::new(environment)
+    }
+
+    #[derive(Debug, uniffi::Object)]
+    pub struct Holder {
+        users: RwLock<Users>,
+    }
+
+    impl Holder {
+        fn read<T: Clone, F>(&self, access: F) -> T
+        where
+            F: Fn(RwLockReadGuard<'_, Users>) -> T,
+        {
+            access(self.users.try_read().unwrap())
+        }
+
+        fn len(&self) -> usize {
+            self.read(|u| u.len())
+        }
+    }
+
+    #[uniffi::export]
+    impl Holder {
+        #[uniffi::constructor]
+        pub fn new(environment: Environment) -> Arc<Self> {
+            Arc::new(Self {
+                users: RwLock::new(Users::new(environment)),
+            })
+        }
+
+        pub fn add_user(&self, named: String) {
+            self.users.try_write().unwrap().add_user(named.as_str())
+        }
+
+        pub fn user_count(&self) -> u32 {
+            self.len() as u32
         }
     }
 }
@@ -54,13 +97,13 @@ uniffi::include_scaffolding!("users");
 
 #[cfg(test)]
 mod test_u0 {
-    use crate::u0::Users;
+    use crate::u0::{Environment, Holder};
 
     #[test]
     fn u0() {
-        let mut users = Users::empty_prod();
-        assert_eq!(users.len(), 0);
-        users.new_user("Foo");
-        assert_eq!(users.len(), 1);
+        let holder = Holder::new(Environment::Prod);
+        assert_eq!(holder.user_count(), 0);
+        holder.add_user("Foo".to_string());
+        assert_eq!(holder.user_count(), 1);
     }
 }
